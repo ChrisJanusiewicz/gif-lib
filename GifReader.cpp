a0487b1130b3262c011790237c7e90beeb14206d
Chris Janusiewicz <kpjanusiewicz@gmail.com>
2020-06-23 18:47:33 +0100

    Fixed inclusion of GifHeader struct

--- GifReader.cpp
+++ GifReader.cpp
@@ -1,6 +1,5 @@
 #include "pch.h"
 #include "GifReader.h"
-#include "GifHeader.h"
 
 #include <iostream>
 
@@ -15,6 +14,53 @@
 
 namespace GifLib {
 
+
+	GifHeader GifHeader::fromStream(std::ifstream& input)
+	{
+		GifHeader header;
+
+		input.read((char*)header.signature, 3);
+		input.read((char*)header.version, 3);
+		input.read((char*)&header.width, 2);
+		input.read((char*)&header.height, 2);
+		input.read((char*)&header.packedFields, 1);
+		input.read((char*)&header.backgroundColorIndex, 1);
+		input.read((char*)&header.pixelAspectRatio, 1);
+
+		return header;
+	}
+
+	bool GifHeader::hasGlobalColorTable()
+	{
+		return packedFields & 0b10000000;
+	}
+
+	bool GifHeader::isSorted()
+	{
+		return packedFields & 0b00001000;
+	}
+
+	size_t GifHeader::getColorResolution()
+	{
+		return ((packedFields & 0b01110000) >> 4) + 1;
+	}
+
+	size_t GifHeader::getGlobalColorTableSize()
+	{
+		return 2 << (packedFields & 0b00000111);
+	}
+
+	void GifHeader::print() {
+		std::cout << "GIF Header: " << signature << version << std::endl;
+		std::cout << "LSD: " << (unsigned short)width << " x " << height << std::endl;
+		std::cout << "Packed: " << (int)packedFields << std::endl;
+		std::cout << "GCT: " << hasGlobalColorTable() << std::endl;
+		std::cout << "Sorted: " << isSorted() << std::endl;
+		std::cout << "Color Res: " << getColorResolution() << std::endl;
+		std::cout << "GCT Size: " << getGlobalColorTableSize() << std::endl;
+		std::cout << "BCI: " << (int)backgroundColorIndex << std::endl;
+		std::cout << "PAR: " << (int)pixelAspectRatio << std::endl;
+	}
 
 	bool ImageDescriptor::hasLocalColorTable()
 	{
